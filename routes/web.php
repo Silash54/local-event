070a6303c17b19a585b346659246ffa1debaa198
Silash54 <silasraii144@gmail.com>
2025-06-23 22:54:58 +0545

    dasboard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\backend\DashboardController;
 use App\Http\Controllers\front\AuthController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,6 @@ Route::get('/', function () {
 //Auth Controller
 Route::get('/login',[AuthController::class,'login'])->name('login');
 Route::get('/register',[AuthController::class,'register'])->name('register');
+
+//Backend dashboard
+Route::get('/dashboard',[DashboardController::class,'dashboard'])->name('dashboard');
